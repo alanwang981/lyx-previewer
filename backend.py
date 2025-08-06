7060cad72e28dc7e1389a672a6a370f174d672a0
rootrc <rootrc123@gmail.com>
2025-08-06 18:24:19 -0400

    conversion from pdf to html (basic)

--- backend.py
+++ backend.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+from spire.pdf import PdfDocument
+from spire.pdf import FileFormat
 
 def lyx_to_pdf(lyx_file):
     lyx_path = "C:/Program Files/LyX 2.4/bin/LyX.exe"
@@ -10,10 +13,25 @@ def lyx_to_pdf(lyx_file):
             stderr = subprocess.PIPE,
             text = True
         )
-        print("Conversion successful!")
     except subprocess.CalledProcessError as e:
         print("ERROR:", e.stderr)
     except FileNotFoundError:
         print("LyX not found at:", lyx_path)
- 
-lyx_to_pdf("C:/test/MCM45 Class 10 Instructor.lyx")
+        
+def pdf_to_html(pdf_file):
+    doc = PdfDocument()
+    doc.LoadFromFile(pdf_file)
+    convertOptions = doc.ConvertOptions
+    convertOptions.SetPdfToHtmlOptions(True, True, 1, True)
+    doc.SaveToFile(pdf_file.replace(".pdf", ".html"), FileFormat.HTML)
+    doc.Dispose()
+
+def lyx_to_html(file_name):
+    if not os.path.exists(file_name):
+        print("File does not exist!")
+        return
+    lyx_to_pdf(file_name)
+    pdf_to_html(file_name.replace(".lyx", ".pdf"))
+    os.remove(file_name.replace(".lyx", ".pdf"))
+    
+lyx_to_html("ME35 Homework 10 Solutions.lyx")
