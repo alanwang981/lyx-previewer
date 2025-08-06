06bb5791b0bb30b58899aea34fdf7350e03cfe2a
rootrc <rootrc123@gmail.com>
2025-08-06 18:25:29 -0400

    partial cleaning of html by removing watermark and centering

--- backend.py
+++ backend.py
@@ -26,6 +26,32 @@ def pdf_to_html(pdf_file):
     doc.SaveToFile(pdf_file.replace(".pdf", ".html"), FileFormat.HTML)
     doc.Dispose()
 
+def clean_html(html_file):
+    warning = (
+        "<tspan xml:space=\"preserve\" style=\"white-space: pre;\" x=\"20\" y=\"20\" "
+        "font-family=\"Arial\" font-weight=\"normal\" font-style=\"normal\" font-size=\"10\" "
+        "fill=\"#ff0000\">Evaluation Warning : The document was created with Spire.PDF for Python.</tspan>"
+    )
+     
+    def insertAfter(haystack, needle, newText):
+        idx = haystack.find(needle)
+        return haystack[:idx + len(needle)] + newText + haystack[idx + len(needle):]
+
+    def insertBefore(haystack, needle, newText):
+        idx = haystack.find(needle)
+        return haystack[:idx] + newText + haystack[idx:]
+    
+    with open(html_file, "r", encoding = 'utf-8') as file:
+        content = file.read()
+        content = content.replace(warning, "")
+        
+        content = insertAfter(content, "</title>", "<style>body{text-align:center;}.centered{margin:auto;width:50%;text-align:center;}</style>")
+        content = insertAfter(content, "<body style='margin:0'>", "<div class='centered'>")
+        content = insertBefore(content, "</body>", "</div>")
+    
+    with open(html_file, "w", encoding = 'utf-8') as file:
+        file.write(content)
+    
 def lyx_to_html(file_name):
     if not os.path.exists(file_name):
         print("File does not exist!")
@@ -33,5 +59,6 @@ def lyx_to_html(file_name):
     lyx_to_pdf(file_name)
     pdf_to_html(file_name.replace(".lyx", ".pdf"))
     os.remove(file_name.replace(".lyx", ".pdf"))
+    clean_html(file_name.replace(".lyx", ".html"))
     
 lyx_to_html("ME35 Homework 10 Solutions.lyx")
